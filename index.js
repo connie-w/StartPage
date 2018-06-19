617fa134915324b701a6892cc7bc79174c2595d5
Connie Wang <zhiyunw@uw.edu>
2018-06-19 23:21:43 +0000

    websites

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var websites = 
+{
+    "websites": [
+        {"social":[
+            {"name": "facebook", "url": "https://www.facebook.com/", "shortcut": "f"},
+            {"name": "reddit", "url": "https://www.reddit.com/", "shortcut": "r"},
+            {"name": "twitter", "url": "https://twitter.com/", "shortcut": "t"},
+            {"name": "tumblr", "url": "https://tumblr.com/", "shortcut": "T"}
+        ]},
+        {"google":[
+            {"name": "google", "url": "https://www.google.com/", "shortcut": "g"},
+            {"name": "youtube", "url": "https://www.youtube.com/", "shortcut": "y"},
+            {"name": "drive", "url": "https://drive.google.com/", "shortcut": "d"},
+            {"name": "gmail", "url": "https://mail.google.com/", "shortcut": "m"}
+        ]}
+    ]
+};
+
+window.onload = function() {
+    for(let i = 0; i < websites.websites.length; i++) {
+        
+    }
+};
+
+
+
