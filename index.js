17308101fba81ff56a4350ea4828953155342249
Connie Wang <zhiyunw@uw.edu>
2018-06-20 00:46:09 +0000

    styling and animations

--- index.js
+++ index.js
@@ -3,26 +3,62 @@
 var websites = 
 {
     "websites": [
-        {"social":[
+        [
             {"name": "facebook", "url": "https://www.facebook.com/", "shortcut": "f"},
             {"name": "reddit", "url": "https://www.reddit.com/", "shortcut": "r"},
             {"name": "twitter", "url": "https://twitter.com/", "shortcut": "t"},
             {"name": "tumblr", "url": "https://tumblr.com/", "shortcut": "T"}
-        ]},
-        {"google":[
+        ],
+        [
             {"name": "google", "url": "https://www.google.com/", "shortcut": "g"},
             {"name": "youtube", "url": "https://www.youtube.com/", "shortcut": "y"},
             {"name": "drive", "url": "https://drive.google.com/", "shortcut": "d"},
             {"name": "gmail", "url": "https://mail.google.com/", "shortcut": "m"}
-        ]}
+        ],
+        [
+            {"name": "github", "url": "https://github.com/", "shortcut": "h"},
+            {"name": "gitlab", "url": "https://gitlab.com/", "shortcut": "l"},
+            {"name": "w3schools", "url": "https://www.w3schools.com/", "shortcut": "w"},
+            {"name": "stackoverflow", "url": "https://stackoverflow.com/", "shortcut": "s"}
+        ]
     ]
 };
 
+var categories = ["social", "google", "code"];
+
 window.onload = function() {
-    for(let i = 0; i < websites.websites.length; i++) {
-        
-    }
+    populateShortcuts();
 };
+
+// Populates page with website shortcuts, organized by categories
+function populateShortcuts() {
+    for(let i = 0; i < websites.websites.length; i++) {
+        let div = document.createElement("div");
+        div.classList.add("category");
+        let category = document.createElement("h3");
+        category.innerHTML = categories[i];
+        div.appendChild(category);
+        
+        for(let j = 0; j < websites.websites[i].length; j++) {
+            let curr = websites.websites[i][j];
+            let p = document.createElement("p");
+            let icon = document.createElement("span");
+            icon.innerText = curr.shortcut;
+            p.appendChild(icon);
+            let text = document.createElement("a");
+            text.innerText = curr.name;
+            text.href = curr.url;
+            p.appendChild(text);
+            div.appendChild(p);
+        }
+        
+        $("shortcut-area").appendChild(div);
+    }   
+}
+
+function $(id) {
+    return document.getElementById(id);
+}
 
 
 
